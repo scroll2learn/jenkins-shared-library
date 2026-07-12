c9a705debcc444caa0e7e5e0172876cc7ae7b901
scroll2learn <73377680+scroll2learn@users.noreply.github.com>
2026-07-12 19:24:52 +0530

    Add deployment function for specified environment

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,1 +1,9 @@
+def call(String environmentName = "production") {
+    echo "Deploying the application to ${environmentName}"
 
+    sh '''
+        echo "Deploying command is running"
+        echo "Environment " ${environmentName}"
+        echo "Deployment Completed"
+        '''
+}
